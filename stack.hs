65a94074bff74e37ad0bdf27553477dfcf403f24
Dan Albert <dan@gingerhq.net>
2013-04-29 19:25:34 -0700

    Implemented part 2

--- stack.hs
+++ stack.hs
@@ -6,21 +6,30 @@ data Cmd = LD Int
          | ADD
          | MULT
          | DUP
+         | DEF String Prog
+         | CALL String
+         deriving Show
 
 type Stack = [Int]
-type StackErr = Maybe Stack
-type D = StackErr -> StackErr
+type Macros = [(String, Prog)]
+type State = Maybe (Macros, Stack)
+
+type D = State -> State
 
 semCmd :: Cmd -> D
-semCmd (LD n) (Just stack)                = Just (n:stack)
-semCmd ADD    (Just (first:second:stack)) = Just (first + second:stack)
-semCmd MULT   (Just (first:second:stack)) = Just (first * second:stack)
-semCmd DUP    (Just (n:stack))            = Just (n:n:stack)
-semCmd _      _                           = Nothing
+semCmd (LD n)    (Just (m, s))     = Just (m, n:s)
+semCmd ADD       (Just (m, x:y:s)) = Just (m, x + y:s)
+semCmd MULT      (Just (m, x:y:s)) = Just (m, x * y:s)
+semCmd DUP       (Just (m, n:s))   = Just (m, n:n:s)
+semCmd (DEF n p) (Just (m, s))     = Just ((n, p):m, s)
+semCmd (CALL n)  (Just (m, s))     = case lookup n m of
+                                       Just prog -> semHelper prog (Just (m, s))
+                                       _         -> Nothing
+semCmd _         _                 = Nothing
 
 semHelper :: Prog -> D
-semHelper [] s     = s
+semHelper []     s = s
 semHelper (c:cs) s = semHelper cs (semCmd c s)
 
-sem :: Prog -> StackErr
-sem prog = semHelper prog (Just [])
+sem :: Prog -> State
+sem prog = semHelper prog (Just ([], []))

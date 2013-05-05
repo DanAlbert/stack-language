56a7848a8233eed8596e46c2733b7cbbbac5b55f
Dan Albert <dan@gingerhq.net>
2013-05-05 12:02:35 -0700

    Added INC, SWAP and POP

--- stack.hs
+++ stack.hs
@@ -6,6 +6,9 @@ data Cmd = LD Int
          | ADD
          | MULT
          | DUP
+         | INC
+         | SWAP
+         | POP Int
          | DEF String Prog
          | CALL String
          deriving Show
@@ -21,6 +24,9 @@ semCmd (LD n)    (Just (m, s))     = Just (m, n:s)
 semCmd ADD       (Just (m, x:y:s)) = Just (m, x + y:s)
 semCmd MULT      (Just (m, x:y:s)) = Just (m, x * y:s)
 semCmd DUP       (Just (m, n:s))   = Just (m, n:n:s)
+semCmd INC       (Just (m, n:s))   = Just (m, (n + 1):s)
+semCmd SWAP      (Just (m, x:y:s)) = Just (m, y:x:s)
+semCmd (POP n)   (Just (m, s))     = Just (m, drop n s)
 semCmd (DEF n p) (Just (m, s))     = Just ((n, p):m, s)
 semCmd (CALL n)  (Just (m, s))     = case lookup n m of
                                        Just prog -> semHelper prog (Just (m, s))

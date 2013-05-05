099350d0e3212806e5b18ff6346210882d344ea5
Dan Albert <dan@gingerhq.net>
2013-05-05 12:54:48 -0700

    Implemented type checking evaluation

--- stack.hs
+++ stack.hs
@@ -17,6 +17,30 @@ type Stack = [Int]
 type Macros = [(String, Prog)]
 type State = Maybe (Macros, Stack)
 
+type D = State -> State
+
+semCmd :: Cmd -> D
+semCmd (LD n)    (Just (m, s))     = Just (m, n:s)
+semCmd ADD       (Just (m, x:y:s)) = Just (m, x + y:s)
+semCmd MULT      (Just (m, x:y:s)) = Just (m, x * y:s)
+semCmd DUP       (Just (m, n:s))   = Just (m, n:n:s)
+semCmd INC       (Just (m, n:s))   = Just (m, (n + 1):s)
+semCmd SWAP      (Just (m, x:y:s)) = Just (m, y:x:s)
+semCmd (POP n)   (Just (m, s))     = Just (m, drop n s)
+semCmd (DEF n p) (Just (m, s))     = Just ((n, p):m, s)
+semCmd (CALL n)  (Just (m, s))     = case lookup n m of
+                                       Just prog -> semHelper prog (Just (m, s))
+                                       _         -> Nothing
+semCmd _         _                 = Nothing
+
+semHelper :: Prog -> D
+semHelper []     s = s
+semHelper (c:cs) s = semHelper cs (semCmd c s)
+
+sem :: Prog -> State
+sem prog = semHelper prog (Just ([], []))
+
+-- Part 1a
 type Rank = Int
 type CmdRank = (Int,Int)
 
@@ -40,25 +64,14 @@ rank (c:cs) k | k >= n    = rank cs r'
 rankP :: Prog -> Maybe Rank
 rankP cs = rank cs 0
 
-type D = State -> State
+-- Part 1b
+semStatTC :: Prog -> State 
+semStatTC prog | rankP prog /= Nothing = sem prog
+               | otherwise             = Nothing
 
-semCmd :: Cmd -> D
-semCmd (LD n)    (Just (m, s))     = Just (m, n:s)
-semCmd ADD       (Just (m, x:y:s)) = Just (m, x + y:s)
-semCmd MULT      (Just (m, x:y:s)) = Just (m, x * y:s)
-semCmd DUP       (Just (m, n:s))   = Just (m, n:n:s)
-semCmd INC       (Just (m, n:s))   = Just (m, (n + 1):s)
-semCmd SWAP      (Just (m, x:y:s)) = Just (m, y:x:s)
-semCmd (POP n)   (Just (m, s))     = Just (m, drop n s)
-semCmd (DEF n p) (Just (m, s))     = Just ((n, p):m, s)
-semCmd (CALL n)  (Just (m, s))     = case lookup n m of
-                                       Just prog -> semHelper prog (Just (m, s))
-                                       _         -> Nothing
-semCmd _         _                 = Nothing
-
-semHelper :: Prog -> D
-semHelper []     s = s
-semHelper (c:cs) s = semHelper cs (semCmd c s)
-
-sem :: Prog -> State
-sem prog = semHelper prog (Just ([], []))
+{-
+ - sem can be simplified by removing all the maybes since we are already doing
+ - type checking before hand. Any call to sem that would result in an error is
+ - already being pruned out of execution by the type checker. The new type would
+ - be Prog -> (Macros, Stack) instead of Prog -> (Maybe (Macros, Stack)).
+ -}

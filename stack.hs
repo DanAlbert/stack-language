8f0eb3c0cc16456aeea243f2729e28f005499800
Dan Albert <dan@gingerhq.net>
2013-05-05 12:32:50 -0700

    Added command and stack ranking for type checking

--- stack.hs
+++ stack.hs
@@ -17,6 +17,29 @@ type Stack = [Int]
 type Macros = [(String, Prog)]
 type State = Maybe (Macros, Stack)
 
+type Rank = Int
+type CmdRank = (Int,Int)
+
+rankC :: Cmd -> CmdRank
+rankC (LD n)  = (0,1)
+rankC ADD     = (2,1)
+rankC MULT    = (2,1)
+rankC DUP     = (1,2)
+rankC INC     = (1,1)
+rankC SWAP    = (2,2)
+rankC (POP n) = (n,0)
+rankC _       = (0,0) -- def and call do nothing
+
+rank :: Prog -> Rank -> Maybe Rank
+rank []     k             = Just k
+rank (c:cs) k | k >= n    = rank cs r'
+              | otherwise = Nothing
+              where (n, m) = rankC c
+                    r'     = k - n + m
+
+rankP :: Prog -> Maybe Rank
+rankP cs = rank cs 0
+
 type D = State -> State
 
 semCmd :: Cmd -> D
